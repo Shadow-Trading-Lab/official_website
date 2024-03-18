78a1b97eebfcc7e312c286a629ff6791fd6470f7
vwvwMM <vincentwu0628@gmail.com>
2024-03-19 00:57:36 +0800

    fill header

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  async redirects() {
+    return [
+        {
+            source: '/',
+            destination: '/index.html',
+            permanent: true,
+        },
+    ];
+  },
 };
 
 export default nextConfig;
